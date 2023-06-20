7154f79b8f0082ccd6e95e83914fb193c003d1e4
Chantal Goethals <chantalgoethals@gmail.com>
2023-06-20 17:31:59 -0600

    Feat: add event listener

---
+++ index.js
@@ -0,0 +1,13 @@
+//$(selector).action()
+
+$(document).ready(function(){
+    e.preventDefault();
+
+    $(".btn").click(function(){
+        $("form").hide();
+    });
+});
+
+// $('form').submit(function(e) {
+//     e.preventDefault();
+//   });
